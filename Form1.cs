4d21edb99926fc927103740288f057c68c81ba97
AdonaiSoaresM <adonaielite@gmail.com>
2024-02-08 22:38:58 -0300

    commit

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_video
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog()==DialogResult.OK)
+            {
+                label2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var handleM3U8 = new HandleM3U8(textBox1.Text);
+            handleM3U8.ProgressoDownload += (progresso) =>
+            {
+                    progressBar1.Value = (int)progresso;
+            };
+
+            var filePath = await handleM3U8.Download(folderBrowserDialog1.SelectedPath);
+
+            await handleM3U8.DownloadTSFiles(filePath, folderBrowserDialog1.SelectedPath);
+            
+            progressBar1.Value = 0;
+            MessageBox.Show("Download Concluido");
+        }
+    }
+}
---
+++ HandleM3U8.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace form_video
+{
+    public delegate void ProgressoDownload(double progresso);
+    public class HandleM3U8
+    {
+        public event ProgressoDownload ProgressoDownload;
+
+        private readonly HttpClient client;
+        public string Url { get; private set; }
+
+        private double _progresso;
+        public double Progresso { get => _progresso ; private set
+            {
+                _progresso = value;
+                ProgressoDownload?.Invoke(_progresso);
+            } }
+
+        private Regex regex = new Regex(@"^video\d+\.ts$");
+        public HandleM3U8(string url)
+        {
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Add("User-Agent", "xrl-enconded");
+
+            Url = url;
+        }
+
+        public async Task<string> Download(string path, string fileName = null, bool fileTs = false)
+        {
+            var url = fileTs ? Url.Replace("video.m3u8","") + fileName : Url;
+            var file = await client.GetStreamAsync(url);
+
+            var filePath = fileName != null ? Path.Combine(path, fileName) : Path.Combine(path, "video.m3u8");
+
+            using (var stream = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return filePath;
+        }
+
+        public async Task DownloadTSFiles(string filePathM3u8, string path)
+        {
+                int totalLinhas = File.ReadAllLines(filePathM3u8).Length;
+                var sr = new StreamReader(filePathM3u8);
+                string linha;
+                int numLinhas = 0;
+                
+                while ((linha = sr.ReadLine()) != null)
+                {
+                numLinhas++;
+                Progresso = (double)numLinhas / totalLinhas * 100;
+
+                    var isMatch = regex.IsMatch(linha);
+                    if (isMatch)
+                    {
+                        await Download(path, linha, true);
+                    }
+                }
+                sr.Close();
+        }
+    }
+}
